88523522dd466f0fd6b863d4c3a0d4179ef8544a
Mạnh Minh <minhmanh.lee@gmail.com>
2023-04-17 10:53:27 +0700

    eva

---
+++ src/test/java/com/evadeeva/eva/EvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evadeeva.eva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
